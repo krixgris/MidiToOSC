71bca9f6d8012fd9aed46c8d901c4317254d1a95
krixgris <83347557+krixgris@users.noreply.github.com>
2021-04-30 06:58:52 +0200

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+#this is launched on reboot after 60 seconds by this statement in crontab
+#@reboot sleep 60 && sh /home/pi/MidiToOSC/launcher.sh >/home/pi/logs/cronlog 2>&1
+
+#to edit this behaviour, use sudo crontab -e
+#to kill the script, you need to either check ps -A for running python events and kill those
+#or for an easier view, something like ps-ejH or pstree or ps axjf
+#or implement a midi listen thing that can stop the script with midi in
+
+
+cd /
+cd home/pi/MidiToOSC/
+sudo python miditoosc.py
+cd /
